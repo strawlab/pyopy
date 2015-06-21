924f63f8d5c0779df85588385ee55224bfd46a78
santi <sdvillal@gmail.com>
2015-06-21 20:15:31 +0200

    do not forget easy solution for EOF struggles

--- pyopy/code.py
+++ pyopy/code.py
@@ -36,7 +36,7 @@ def parse_matlab_funcdef(mfile,
     """
     expected_func_name = op.splitext(op.basename(mfile))[0]
     with open(mfile) as reader:
-        text = reader.read()
+        text = reader.read()  # .replace('\r\n', '\n')
         doc, out, funcname, parameters, code = funcdef_pattern.split(text, maxsplit=1)
         if not funcname == expected_func_name:
             raise Exception('Problem parsing %s.\n'
